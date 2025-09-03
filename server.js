13508b299ce235ed635e0363442e368734be0826
Javed Akhtar <javed09523@gmail.com>
2025-09-03 08:48:37 +0530

    server js me cors changes

--- server.js
+++ server.js
@@ -10,7 +10,17 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://jobportalfrontend-delta.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 // mongoose
 //   .connect(process.env.MONGO_URI, {
